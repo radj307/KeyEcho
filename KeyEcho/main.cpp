2554b3e920af5231fd4746aed2536ed40beaf721
radj307 <radj307@gmail.com>
2022-01-06 14:10:46 -0500

    initial content commit

---
+++ KeyEcho/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <make_exception.hpp>
+
+int main(const int argc, char** argv)
+{
+	try {
+
+		return 0;
+	} catch (const std::exception& ex) {
+		std::cerr << ex.what() << std::endl;
+		return -1;
+	} catch (...) {
+		std::cerr << "An unknown error occurred!" << std::endl;
+		return -1;
+	}
+}
